6d8913482c520b654b404b4a9a7e14cfd2bb5b9c
othaderek <otha.hernandez@gmail.com>
2021-03-17 22:42:00 -0400

    Added All LinkedList method shells. Needs implementation.

--- DataStructures/LinkedList.cs
+++ DataStructures/LinkedList.cs
@@ -12,10 +12,11 @@ namespace DSA
         public void IncrementSize() => this.Size += 1;
         public void DecrementSize() => this.Size -= 1;
 
+        // Push: Add to end of LL
+        // Check if there is a head
+        // If there is no head add new node to head and tail increment ll
+        // If there is already a head then set ll node to next of the tail and then reset tail to newNode
         public void Push(int val){
-            // Check if there is a head
-            // If there is no head add new node to head and tail increment ll
-            // If there is already a head then set ll node to next of the tail and then reset tail to newNode
             LinkedListNode newNode = new LinkedListNode(val);
             if (this.Size == 0)
             {
@@ -28,12 +29,28 @@ namespace DSA
             this.Tail = newNode;
             this.IncrementSize();
         }
-        // Push: Add to end of LL
         // Pop: Remove from end of LL
+        public void Pop(){
+
+        }
         // Insert: Given a value and a position insert a new linked list node of given value at position
+        public void Insert(int val){
+
+        }
         // Delete: Find LL node with given value and delete
-        // Traverse: 
+        public void Delete(int val){
+
+        }
+        // Traverse:
+        public LinkedListNode[] Traverse(){
+            LinkedListNode[] allNodes = {};
+            return allNodes;
+        }
         // Reverse: 
+        public LinkedListNode[] Reverse(){
+            LinkedListNode[] reversedNodes = {};
+            return reversedNodes;
+        }
 
     }
 }
