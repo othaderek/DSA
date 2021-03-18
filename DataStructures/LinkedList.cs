fd6f71ec79d3d07e383bc4eb66456e0b024fc79c
othaderek <otha.hernandez@gmail.com>
2021-03-17 22:34:52 -0400

    Added Push method to class.

--- DataStructures/LinkedList.cs
+++ DataStructures/LinkedList.cs
@@ -4,25 +4,29 @@ namespace DSA
 {
     public class LinkedList
     {
-        private LinkedListNode head;
-        private LinkedListNode tail;
-        private int size;
-
-        public LinkedList(){
-            this.size = 0;
-        }
 
         public LinkedListNode Head { get; set; }
         public LinkedListNode Tail { get; set; }
         public int Size { get; set; }
 
-        public int IncermentSize(){
-            this.size += 1;
-            return this.size;
-        }
-        public int DecrementSize(){
-            this.size -= 1;
-            return this.size;
+        public void IncrementSize() => this.Size += 1;
+        public void DecrementSize() => this.Size -= 1;
+
+        public void Push(int val){
+            // Check if there is a head
+            // If there is no head add new node to head and tail increment ll
+            // If there is already a head then set ll node to next of the tail and then reset tail to newNode
+            LinkedListNode newNode = new LinkedListNode(val);
+            if (this.Size == 0)
+            {
+                this.Head = newNode;
+                this.Tail = newNode;
+                this.IncrementSize();
+                return;
+            }
+            this.Tail.Next = newNode;
+            this.Tail = newNode;
+            this.IncrementSize();
         }
         // Push: Add to end of LL
         // Pop: Remove from end of LL
