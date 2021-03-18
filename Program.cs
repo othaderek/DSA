c88508934b56ad8033a9f3bfc56e10b59f53c087
othaderek <otha.hernandez@gmail.com>
2021-03-17 21:28:53 -0400

    Init.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListNode n1 = new LinkedListNode(1);
+            Console.WriteLine($"This is a Linked List Node with the value of {n1.Value.ToString()}");
+        }
+    }
+}
+
+/*
+Todo:
+
+
+*/
